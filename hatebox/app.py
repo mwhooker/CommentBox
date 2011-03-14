99e7ad5ad965f4d75ecdbd8c0e0d9dca59259b54
Matthew Hooker <mhooker@digg.com>
2011-03-13 22:33:25 -0400

    hater image

--- hatebox/app.py
+++ hatebox/app.py
@@ -1,10 +1,19 @@
+import random
+import glob
+import os
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+def get_hater_image(image_dir):
+    return random.choice(glob.glob( os.path.join(image_dir, '*') ))
+
+
 @app.route("/")
 def hello():
-    return render_template('main.html')
+    hater = get_hater_image('static/haters')
+    return render_template('main.html', hater=hater)
 
 if __name__ == "__main__":
     app.debug = True
